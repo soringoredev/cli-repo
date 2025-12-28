ad6f486b291c0c0ddcab319fcc6f1cb50e521e31
soringoredev@gmail.com <motanul66@mail.com>
2025-12-28 13:24:51 +0200

    Expose endpoint /team

--- src/main/java/pixel/academy/demo/MagicRestController.java
+++ src/main/java/pixel/academy/demo/MagicRestController.java
@@ -1,6 +1,7 @@
 package pixel.academy.demo;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +17,15 @@ public class MagicRestController {
 
     @Value("${wizard.spell}")
     private String wizardSpell;
+
+
+    // expose endpoint "/team"
+
+    @GetMapping("/team")
+    public String getTeamInfo() {
+        return "Wizard name: "     + wizardName  +
+                " Wizard house: "  + wizardHouse +
+                " Wizard spell: "  + wizardSpell;
+    }
 
 }
